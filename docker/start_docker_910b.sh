80a971cba4cf0d97fc6425ebb0ab9a1c1f255c3d
Anastasios Zouzias <anastasios.zouzias@huawei.com>
2026-08-18 13:36:47 +0000

    fix

---
+++ docker/start_docker_910b.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# start_docker_910B.sh
+#
+# Description:
+#   Start a Docker container with vLLM-ascend.
+#   The script mounts the local cache of the user into the container so that the model weights are not downloaded every time the container is started.
+#
+# Usage:
+#   ./start_docker_910B.sh
+#
+
+DOCKER_IMAGE_TAG="quay.io/ascend/vllm-ascend:v0.19.1rc1"
+
+drun() {
+
+docker run -it --rm --privileged --network=host --ipc=host --shm-size=16g \
+    --device=/dev/davinci0 --device=/dev/davinci1 --device=/dev/davinci2 --device=/dev/davinci3 \
+    --device=/dev/davinci4 --device=/dev/davinci5 --device=/dev/davinci6 --device=/dev/davinci7 \
+    --device=/dev/davinci_manager --device=/dev/hisi_hdc \
+    --volume /usr/local/sbin:/usr/local/sbin --volume /usr/local/Ascend/driver:/usr/local/Ascend/driver \
+    --volume /usr/local/Ascend/firmware:/usr/local/Ascend/firmware \
+    --volume /etc/ascend_install.info:/etc/ascend_install.info \
+    --name vLLM-ascend-${USER} \
+    --volume /var/queue_schedule:/var/queue_schedule --volume ~/.cache/:/root/.cache/ "$@"
+}
+
+drun "$@" --env "HF_ENDPOINT=https://hf-mirror.com" ${DOCKER_IMAGE_TAG} /usr/bin/bash
